6aac7875753d3f832bf935a8913a8ab75852aa97
taoufik ahjam <taoufikahjam@gmail.com>
2022-01-14 15:08:39 +0100

    add about page

---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const about = () => {
+    return (
+        <div>
+            <h1>about PAge</h1>
+            <p>Duis labore sit cillum eu exercitation dolor mollit ut minim cupidatat.</p>
+        </div>
+    )
+}
+
+export default about
